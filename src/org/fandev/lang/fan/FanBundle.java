e8b16d400921c3fa4c720e5e06c2dd4b755ab93f
VISTALL <vistall.valeriy@gmail.com>
2017-01-23 20:19:30 +0300

    compilation

--- src/org/fandev/lang/fan/FanBundle.java
+++ src/org/fandev/lang/fan/FanBundle.java
@@ -1,8 +1,24 @@
 package org.fandev.lang.fan;
 
-import consulo.lombok.annotations.Bundle;
+import org.jetbrains.annotations.PropertyKey;
+import com.intellij.AbstractBundle;
 
-@Bundle
-public class FanBundle
+public class FanBundle extends AbstractBundle
 {
+	private static final FanBundle ourInstance = new FanBundle();
+
+	private FanBundle()
+	{
+		super("messages.FanBundle");
+	}
+
+	public static String message(@PropertyKey(resourceBundle = "messages.FanBundle") String key)
+	{
+		return ourInstance.getMessage(key);
+	}
+
+	public static String message(@PropertyKey(resourceBundle = "messages.FanBundle") String key, Object... params)
+	{
+		return ourInstance.getMessage(key, params);
+	}
 }
--- src/org/fandev/lang/fan/psi/FanType.java
+++ src/org/fandev/lang/fan/psi/FanType.java
@@ -1,14 +1,26 @@
 package org.fandev.lang.fan.psi;
 
-import consulo.lombok.annotations.ArrayFactoryFields;
+import org.jetbrains.annotations.NotNull;
+import com.intellij.util.ArrayFactory;
 
 /**
  * @author VISTALL
  * @since 04.02.14
  */
-@ArrayFactoryFields
 public interface FanType
 {
+	public static final FanType[] EMPTY_ARRAY = new FanType[0];
+
+	public static ArrayFactory<FanType> ARRAY_FACTORY = new ArrayFactory<FanType>()
+	{
+		@NotNull
+		@Override
+		public FanType[] create(int count)
+		{
+			return count == 0 ? EMPTY_ARRAY : new FanType[count];
+		}
+	};
+
 	FanType BOTTOM = new FanType()
 	{
 		@Override
